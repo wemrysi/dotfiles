2169c72ea41028d985be31729cb631881bca342c
Emrys Ingersoll <ingersoll@gmail.com>
2026-05-24 17:31:48 -0700

    fix: replace pamixer poll with xmobar Volume plugin
    pamixer --get-volume-human was being exec'd every second (interval=10),
    opening a fresh PulseAudio connection each time. This caused pipewire-pulse
    to emit 'no peercred: Bad file descriptor' spam at 1Hz.
    
    Switch to xmobar's built-in Volume plugin which holds a single ALSA handle
    open permanently - no repeated PulseAudio connections.

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -11,7 +11,7 @@ Config {
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Wireless "wlp0s20f3" ["-t","<essid>","-H","80","-L","40","-l","#FFB6B0","-h","#CEFFAC","-n","#FFFFCC"] 50,
         Run Network "wlp0s20f3" ["-t","<rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Com "pamixer" ["--get-volume-human"] "vol" 10,
+        Run Volume "default" "Master" ["-t","<volume>%","--","-O","<volume>%","-o","muted"] 10,
         Run Brightness ["-t","Lux: <percent>%","--","-D","intel_backlight"] 10,
         Run Battery ["-t","Bat: <left>% (<timeleft>)","-L","20","-H","80","-h","#CEFFAC","-l","#FFB6B0","-n","#FFFFCC"] 60,
         Run Date "%a %b %_d %H:%M" "date" 10,
@@ -19,5 +19,5 @@ Config {
     ],
     sepChar  = "%",
     alignSep = "}{",
-    template = "%XMonadLog% }{ %cpu%   %memory%   %wlp0s20f3wi%: %wlp0s20f3%   Vol: %vol%   %bright%   %battery%   <fc=#FFFFCC>%date%</fc>   %KPSN%"
+    template = "%XMonadLog% }{ %cpu%   %memory%   %wlp0s20f3wi%: %wlp0s20f3%   Vol: %default:Master%   %bright%   %battery%   <fc=#FFFFCC>%date%</fc>   %KPSN%"
 }
